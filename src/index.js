d7f1ce42617d266bbee92a161a4e98609210fe10
Rayan84 <rayan_84@live.com>
2021-10-08 11:48:06 +0200

    Add function createGame

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+const baseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/'
+const scoresURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Ahr8VUxI7b1t5hzEwQCC/scores'
+
+
+const createGame = () => {
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST',  `${baseURL}games/`);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    'name': 'my cool new game'
+    })
+  )
+  .then = () => console.log('Game created...');
+}
+
+createGame();
